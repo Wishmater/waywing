1043e2ad0812164a7466d0f39410f9343909a4cd
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-11 15:41:32 -0500

    Add support to all dockedSize to DockedRoundedCornersClipper

--- lib/gui/widgets/docked_rounded_corners_clipper.dart
+++ lib/gui/widgets/docked_rounded_corners_clipper.dart
@@ -34,21 +34,63 @@ class DockedRoundedCornersClipper extends CustomClipper<Path> {
       radiusInY = height * radiusInPercCross;
     }
 
-    // Top-left corner
-    path.moveTo(0, radiusInY);
-    path.quadraticBezierTo(0, radiusOutY, radiusInX, radiusOutY);
+    switch (dockedSide) {
+      case ScreenEdge.right:
+        // Top-left corner
+        path.moveTo(0, radiusInY);
+        path.quadraticBezierTo(0, radiusOutY, radiusInX, radiusOutY);
+        // Top-right corner
+        path.lineTo(width - radiusOutX, radiusOutY);
+        path.quadraticBezierTo(width, radiusOutY, width, 0);
+        // Bottom-right corner
+        path.lineTo(width, height);
+        path.quadraticBezierTo(width, height - radiusOutY, width - radiusOutX, height - radiusOutY);
+        // Bottom-left corner
+        path.lineTo(radiusInX, height - radiusOutY);
+        path.quadraticBezierTo(0, height - radiusOutY, 0, height - radiusInY);
 
-    // Top-right corner
-    path.lineTo(width - radiusOutX, radiusOutY);
-    path.quadraticBezierTo(width, radiusOutY, width, 0);
+      case ScreenEdge.left:
+        // Top-left corner
+        path.moveTo(0, 0);
+        path.quadraticBezierTo(0, radiusOutY, radiusOutX, radiusOutY);
+        // Top-right corner
+        path.lineTo(width - radiusInX, radiusOutY);
+        path.quadraticBezierTo(width, radiusOutY, width, radiusInY);
+        // Bottom-right corner
+        path.lineTo(width, height - radiusInY);
+        path.quadraticBezierTo(width, height - radiusOutY, width - radiusInX, height - radiusOutY);
+        // Bottom-left corner
+        path.lineTo(radiusOutX, height - radiusOutY);
+        path.quadraticBezierTo(0, height - radiusOutY, 0, height);
 
-    // Bottom-right corner
-    path.lineTo(width, height);
-    path.quadraticBezierTo(width, height - radiusOutY, width - radiusOutX, height - radiusOutY);
+      case ScreenEdge.top:
+        // Top-left corner
+        path.moveTo(radiusOutX, radiusOutY);
+        path.quadraticBezierTo(radiusOutX, 0, 0, 0);
+        // Top-right corner
+        path.lineTo(width, 0);
+        path.quadraticBezierTo(width - radiusOutX, 0, width - radiusOutX, radiusOutY);
+        // Bottom-right corner
+        path.lineTo(width - radiusOutX, height - radiusInY);
+        path.quadraticBezierTo(width - radiusOutX, height, width - radiusInX, height);
+        // Bottom-left corner
+        path.lineTo(radiusInX, height);
+        path.quadraticBezierTo(radiusOutX, height, radiusOutX, height - radiusInY);
 
-    // Bottom-left corner
-    path.lineTo(radiusInX, height - radiusOutY);
-    path.quadraticBezierTo(0, height - radiusOutY, 0, height - radiusInY);
+      case ScreenEdge.bottom:
+        // Top-left corner
+        path.moveTo(0, height);
+        path.quadraticBezierTo(radiusOutX, height, radiusOutX, height - radiusOutY);
+        // Top-right corner
+        path.lineTo(radiusOutX, radiusInY);
+        path.quadraticBezierTo(radiusOutX, 0, radiusInX, 0);
+        // Bottom-right corner
+        path.lineTo(width - radiusInX, 0);
+        path.quadraticBezierTo(width - radiusOutX, 0, width - radiusOutX, radiusInY);
+        // Bottom-left corner
+        path.lineTo(width - radiusOutX, height - radiusOutY);
+        path.quadraticBezierTo(width - radiusOutX, height, width, height);
+    }
 
     path.close();
     return path;

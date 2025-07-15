8f802581afe550f8d6a14bf78beacc4ae574c4d9
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-14 23:02:28 -0500

    Refactor config to make it closer to its final implementation

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element_parameter  TODO: 2 remove this once reading user config is implemented, which will use all params
+
 import 'package:fl_linux_window_manager/models/screen_edge.dart';
 import 'package:flutter/material.dart';
 import 'package:waywing/core/feather.dart';
@@ -8,49 +10,104 @@ late Config _config;
 
 @immutable
 class Config {
-  Config._();
-
   // Theme / styling
-  final ThemeMode themeMode = ThemeMode.light;
-  final Color seedColor = Colors.deepPurple;
+  final ThemeMode themeMode;
+  final Color seedColor;
 
   // Animations
-  final Duration animationDuration = const Duration(milliseconds: 250);
-  final Curve animationCurve = Curves.easeOutCubic;
+  final Duration animationDuration;
+  final Curve animationCurve;
   // TODO: 2 we probably want to set different animation "types" and then the user can set duration and curve for each of them
 
+  // Layer settings
+  final double? exclusiveSizeLeft;
+  final double? exclusiveSizeRight;
+  final double? exclusiveSizeTop;
+  final double? exclusiveSizeBottom;
+  // Note (add to readme when it exists): explicitly set exclusiveSice will have priority over Bar size.
+  // Set exclusiveSize to zero on same side bar is on to remove autoExclusiveSize on Bar.
+  double? getExclusiveSizeForSide(ScreenEdge side) {
+    return switch (side) {
+      ScreenEdge.left => exclusiveSizeLeft,
+      ScreenEdge.right => exclusiveSizeRight,
+      ScreenEdge.top => exclusiveSizeTop,
+      ScreenEdge.bottom => exclusiveSizeBottom,
+    };
+  }
+
   // Bar positioning / sizing
-  final ScreenEdge barSide = ScreenEdge.right;
-  final int barWidth = 64; // in pixels
-  final double barMarginLeft = 0; // in flutter DIP, maybe also make in pixels so it's consistent
-  final double barMarginRight = 0; // in flutter DIP, maybe also make in pixels so it's consistent
-  final double barMarginTop = 380; // in flutter DIP, maybe also make in pixels so it's consistent
-  final double barMarginBottom = 340; // in flutter DIP, maybe also make in pixels so it's consistent
-  final double barItemSize = 64; // in flutter DIP, maybe also make in pixels so it's consistent
+  final ScreenEdge barSide;
+  final int barWidth; // in pixels
+  final double barMarginLeft; // in flutter DIP, maybe also make in pixels so it's consistent
+  final double barMarginRight; // in flutter DIP, maybe also make in pixels so it's consistent
+  final double barMarginTop; // in flutter DIP, maybe also make in pixels so it's consistent
+  final double barMarginBottom; // in flutter DIP, maybe also make in pixels so it's consistent
+  final double barItemSize; // in flutter DIP, maybe also make in pixels so it's consistent
   // Derivates
   late final bool isBarVertical = config.barSide == ScreenEdge.left || config.barSide == ScreenEdge.right;
   // TODO: 3 validate that mainSize is not <=0 after deducting margins
   // TODO: 3 validate that you can't add margin on sides that conflict with barSide selected
 
   // Bar border radius
-  final double barRadiusInPercCross = 0.5; // in percentage of bar cross-size
-  final double barRadiusInPercMain = 0.5 * 0.67; // in percentage of bar cross-size
-  final double barRadiusOutPercCross = 0.5; // in percentage of bar cross-size
-  final double barRadiusOutPercMain = 0.5 * 1.5; // in percentage
+  final double barRadiusInPercCross; // in percentage of bar cross-size
+  final double barRadiusInPercMain; // in percentage of bar cross-size
+  final double barRadiusOutPercCross; // in percentage of bar cross-size
+  final double barRadiusOutPercMain; // in percentage
   // TODO: 2 also support fixed pixel radius values
 
   // Bar feathers (components)
   // When implementing reading config, get the instance with Feather.getByName
-  final List<Feather> barStartFeathers = List.unmodifiable([]);
-  final List<Feather> barCenterFeathers = List.unmodifiable([]);
-  final List<Feather> barEndFeathers = List.unmodifiable([
-    featherRegistry.getFeatherByName('Clock'),
-  ]);
+  final List<Feather> barStartFeathers;
+  final List<Feather> barCenterFeathers;
+  final List<Feather> barEndFeathers;
   // TODO: 3 validate that passed feather names exist
+
+  Config._({
+    this.themeMode = ThemeMode.system,
+    required this.seedColor,
+    this.animationDuration = const Duration(milliseconds: 250),
+    this.animationCurve = Curves.easeOutCubic,
+    double? exclusiveSizeLeft,
+    double? exclusiveSizeRight,
+    double? exclusiveSizeTop,
+    double? exclusiveSizeBottom,
+    required this.barSide,
+    required this.barWidth,
+    double? barItemSize,
+    this.barMarginLeft = 0,
+    this.barMarginRight = 0,
+    this.barMarginTop = 0,
+    this.barMarginBottom = 0,
+    this.barRadiusInPercCross = 0,
+    this.barRadiusInPercMain = 0,
+    this.barRadiusOutPercCross = 0,
+    this.barRadiusOutPercMain = 0,
+    this.barStartFeathers = const [],
+    this.barCenterFeathers = const [],
+    this.barEndFeathers = const [],
+  }) : exclusiveSizeLeft = exclusiveSizeLeft ?? (barSide == ScreenEdge.left ? barWidth.toDouble() : null),
+       exclusiveSizeRight = exclusiveSizeRight ?? (barSide == ScreenEdge.right ? barWidth.toDouble() : null),
+       exclusiveSizeTop = exclusiveSizeTop ?? (barSide == ScreenEdge.top ? barWidth.toDouble() : null),
+       exclusiveSizeBottom = exclusiveSizeBottom ?? (barSide == ScreenEdge.bottom ? barWidth.toDouble() : null),
+       barItemSize = barItemSize ?? barWidth.toDouble();
 }
 
 Future<Config> reloadConfig() async {
   // TODO: 2 get config from user file
-  _config = Config._();
+  _config = Config._(
+    themeMode: ThemeMode.light,
+    seedColor: Colors.blue,
+    barSide: ScreenEdge.right,
+    barWidth: 64,
+    barMarginTop: 380,
+    barMarginBottom: 340,
+    barRadiusInPercCross: 0.5,
+    barRadiusInPercMain: 0.5 * 0.67,
+    barRadiusOutPercCross: 0.5,
+    barRadiusOutPercMain: 0.5 * 1.5,
+    barEndFeathers: List.unmodifiable([
+      featherRegistry.getFeatherByName('Clock'),
+    ]),
+  );
   return _config;
 }

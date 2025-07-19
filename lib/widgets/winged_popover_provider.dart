15a52dbdcafcb718e6055c89b4ff3120bd0f9c58
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-18 21:55:32 -0500

    Add animation when closing a popover

--- lib/widgets/winged_popover_provider.dart
+++ lib/widgets/winged_popover_provider.dart
@@ -21,14 +21,16 @@ class WingedPopoverProvider extends StatefulWidget {
 class WingedPopoverProviderState extends State<WingedPopoverProvider> {
   final Map<String, GlobalKey> containerGlobalKeys = {};
   final Set<WingedPopoverState> activeHosts = {};
+  final Set<WingedPopoverState> removedHosts = {};
 
   void showHost(WingedPopoverState host) {
-    assert(!activeHosts.contains(host), 'Trying to register host that already exists to PopoverProvider.');
+    assert(!activeHosts.contains(host), 'Trying to register a host that already exists to PopoverProvider.');
     // hide other popovers with the same containerId
     if (host.widget.containerId != null) {
       final toRemove = activeHosts.where((e) => e.widget.containerId == host.widget.containerId).toList();
+      toRemove.addAll(removedHosts.where((e) => e.widget.containerId == host.widget.containerId));
       for (final e in toRemove) {
-        hideHost(e);
+        _removeHost(e);
       }
     }
     setState(() {
@@ -38,9 +40,22 @@ class WingedPopoverProviderState extends State<WingedPopoverProvider> {
   }
 
   void hideHost(WingedPopoverState host) {
-    assert(activeHosts.contains(host), 'Trying to unregister that doesn\'t exist in PopoverProvider.');
+    assert(activeHosts.contains(host), 'Trying to hide a host that doesn\'t exist in PopoverProvider.');
     setState(() {
       activeHosts.remove(host);
+      removedHosts.add(host);
+      host.isShown = false;
+    });
+  }
+
+  void _removeHost(WingedPopoverState host) {
+    assert(
+      activeHosts.contains(host) || removedHosts.contains(host),
+      'Trying to remove a host that doesn\'t exist in PopoverProvider.',
+    );
+    setState(() {
+      activeHosts.remove(host);
+      removedHosts.remove(host);
       host.isShown = false;
     });
   }
@@ -57,24 +72,8 @@ class WingedPopoverProviderState extends State<WingedPopoverProvider> {
   Widget build(BuildContext context) {
     final widgetsBelowMap = <Widget, WingedPopoverState>{};
     final widgetsAboveMap = <Widget, WingedPopoverState>{};
-    for (final host in activeHosts) {
-      final Key key;
-      if (host.widget.containerId != null) {
-        containerGlobalKeys[host.widget.containerId!] ??= GlobalKey();
-        key = containerGlobalKeys[host.widget.containerId!]!;
-      } else {
-        key = ValueKey(host);
-      }
-      final widget = _WingedPopoverClient(
-        host: host,
-        key: key,
-      );
-      if (host.widget.zIndex < 0) {
-        widgetsBelowMap[widget] = host;
-      } else {
-        widgetsAboveMap[widget] = host;
-      }
-    }
+    buildClientWidgets(context, activeHosts, widgetsBelowMap, widgetsAboveMap);
+    buildClientWidgets(context, removedHosts, widgetsBelowMap, widgetsAboveMap, isRemoved: true);
     final widgetsBelow = widgetsBelowMap.keys.toList();
     widgetsBelow.sortedBy((e) => widgetsBelowMap[e]!.widget.zIndex);
     final widgetsAbove = widgetsAboveMap.keys.toList();
@@ -87,13 +86,43 @@ class WingedPopoverProviderState extends State<WingedPopoverProvider> {
       ],
     );
   }
+
+  void buildClientWidgets(
+    BuildContext context,
+    Iterable<WingedPopoverState> hosts,
+    Map<Widget, WingedPopoverState> widgetsBelowMap,
+    Map<Widget, WingedPopoverState> widgetsAboveMap, {
+    bool isRemoved = false,
+  }) {
+    for (final host in hosts) {
+      final Key key;
+      if (host.widget.containerId != null) {
+        containerGlobalKeys[host.widget.containerId!] ??= GlobalKey();
+        key = containerGlobalKeys[host.widget.containerId!]!;
+      } else {
+        key = ValueKey(host);
+      }
+      final widget = _WingedPopoverClient(
+        host: host,
+        key: key,
+        isRemoved: isRemoved,
+      );
+      if (host.widget.zIndex < 0) {
+        widgetsBelowMap[widget] = host;
+      } else {
+        widgetsAboveMap[widget] = host;
+      }
+    }
+  }
 }
 
 class _WingedPopoverClient extends StatefulWidget {
   final WingedPopoverState host;
+  final bool isRemoved;
 
   const _WingedPopoverClient({
     required this.host,
+    this.isRemoved = false,
     super.key, // ignore: unused_element_parameter
   });
 
@@ -116,26 +145,31 @@ class _WingedPopoverClientState extends State<_WingedPopoverClient> {
 
     Size childSize;
     Offset childPosition;
-    try {
-      childSize = childPositioningController.getPositioning().$2;
-      childPosition = getPopoverPosition(
-        anchorAlignment: widget.host.widget.anchorAlignment,
-        popupAlignment: widget.host.widget.popupAlignment,
-        hostPosition: hostPosition,
-        hostSize: hostSize,
-        childSize: childSize,
-        screenSize: screenSize,
-        padding: padding,
-      );
-    } catch (_) {
-      // assuming this happens the first time the widget builds
-      // fall back to setting the size/position of the host,
-      // which should have the added effect of animating entry
+    if (widget.isRemoved) {
       childSize = hostSize;
       childPosition = hostPosition;
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        setState(() {});
-      });
+    } else {
+      try {
+        childSize = childPositioningController.getPositioning().$2;
+        childPosition = getPopoverPosition(
+          anchorAlignment: widget.host.widget.anchorAlignment,
+          popupAlignment: widget.host.widget.popupAlignment,
+          hostPosition: hostPosition,
+          hostSize: hostSize,
+          childSize: childSize,
+          screenSize: screenSize,
+          padding: padding,
+        );
+      } catch (_) {
+        // assuming this happens the first time the widget builds
+        // fall back to setting the size/position of the host,
+        // which should have the added effect of animating entry
+        childSize = hostSize;
+        childPosition = hostPosition;
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          setState(() {});
+        });
+      }
     }
     // print('childSize: $childSize');
     // print('childPosition: $childPosition');
@@ -176,7 +210,13 @@ class _WingedPopoverClientState extends State<_WingedPopoverClient> {
       top: childPosition.dy,
       width: childSize.width,
       height: childSize.height,
+      onEnd: !widget.isRemoved ? null : onRemoveAnimationEnd,
       child: container,
     );
+  }
+
+  void onRemoveAnimationEnd() {
+    final provider = context.findAncestorStateOfType<WingedPopoverProviderState>()!;
+    provider._removeHost(widget.host);
   }
 }

1953ff5feb031a0338d3017e73ffea158a44d4e9
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-22 17:25:50 -0500

    Set a specific commit to nix_unstable

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> { }, }:
 
 let
-  unstablenixpkgs = fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  unstablenixpkgs = fetchTarball {
+    url =
+      "https://github.com/NixOS/nixpkgs/archive/bf76d8af397b7a40586b4cbc89dc2e3d2370deaa.tar.gz";
+    sha256 = "0si14qvhc0z0ip14gm9x7fw3bma8z260zf34g2hf54r5jnlnj9r8";
+  };
   unstablepkgs = import unstablenixpkgs {
     config = { };
     overlays = [ ];

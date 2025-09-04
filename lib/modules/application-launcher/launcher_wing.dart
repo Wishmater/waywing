46f6a7338c11db02554f45fe20ebdfe31eaeb4bd
ross96D <abelaross96@gmail.com>
2025-09-04 15:15:52 -0400

    launcher: initial commit

---
+++ lib/modules/application-launcher/launcher_wing.dart
@@ -0,0 +1,13 @@
+
+import "package:flutter/material.dart";
+import "package:waywing/core/wing.dart";
+
+class AppLauncherWing extends Wing {
+  @override
+  Widget buildWing(EdgeInsets rerservedSpace) {
+    throw UnimplementedError();
+  }
+
+  @override
+  String get name => "AppLauncher";
+}

91dc6f0ca2513405bebb0a45aa25e287dddc48f8
ross96D <abelaross96@gmail.com>
2025-09-16 16:43:34 -0400

    utils: improve math utils

--- lib/util/math_utils.dart
+++ lib/util/math_utils.dart
@@ -1,27 +1,9 @@
-T minAbs<T extends num>(T a, T b) {
-  if (a.abs() < b.abs()) {
-    return a;
-  } else {
-    return b;
-  }
-}
+import "dart:math";
 
-T maxAbs<T extends num>(T a, T b) {
-  if (a.abs() > b.abs()) {
-    return a;
-  } else {
-    return b;
-  }
-}
+T minAbs<T extends num>(T a, T b) => min(a.abs() as T, b.abs() as T);
+
+T maxAbs<T extends num>(T a, T b) => max(a.abs() as T, b.abs() as T);
 
 extension ClampNullable<T extends num> on T {
-  T clampNullable(T? a, [T? b]) {
-    if (a != null && a > this) {
-      return a;
-    }
-    if (b != null && b < this) {
-      return b;
-    }
-    return this;
-  }
+  T clampNullable(T? a, [T? b]) => clamp(a ?? -double.maxFinite, b ?? double.maxFinite) as T;
 }

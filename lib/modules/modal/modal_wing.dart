2a3da126dc866af7041809619f15ad8cbcf18111
ross96D <abelaross96@gmail.com>
2025-09-18 09:26:52 -0400

    minor change

--- lib/modules/modal/modal_wing.dart
+++ lib/modules/modal/modal_wing.dart
@@ -1,5 +1,4 @@
-import "package:flutter/src/painting/edge_insets.dart";
-import "package:flutter/src/widgets/framework.dart";
+import "package:flutter/widgets.dart";
 import "package:waywing/core/feather_registry.dart";
 import "package:waywing/core/wing.dart";
 

70b9379eafc7cc491426ef886b1b125b6d4c522d
ross96D <abelaross96@gmail.com>
2025-09-21 08:43:03 -0400

    add shape shadow painter implementation

--- lib/widgets/shapes/shape_clipper.dart
+++ lib/widgets/shapes/shape_clipper.dart
@@ -1,5 +1,6 @@
 import "dart:math";
 
+import "package:flutter/material.dart";
 import "package:flutter/widgets.dart";
 
 class ShapeClipper extends CustomClipper<Path> {
@@ -57,4 +58,53 @@ class ShapeShadowClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(ShapeShadowClipper oldClipper) => shape != oldClipper.shape;
+}
+
+/// TODO create a benchmark to compare implementations
+class ShapeShadowPainter extends CustomPainter {
+  final ShapeBorder shape;
+  final Shadow shadow;
+  final Rect? rectOverride;
+
+  const ShapeShadowPainter({required this.shape, required this.shadow, this.rectOverride});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    final Path innerPath = shape.getOuterPath(rect);
+
+    final double dx = shadow.offset.dx;
+    final double dy = shadow.offset.dy;
+    final int steps = max(dx.abs(), dy.abs()).ceil();
+
+    if (steps == 0) {
+      // No offset, draw the original path
+      canvas.drawPath(innerPath, shadow.toPaint());
+      return;
+    }
+
+    // Create a union of all incremental offset paths
+    // Path totalPath = innerPath;
+    // for (int i = 1; i <= steps; i++) {
+    //   final stepOffset =  shadow.offset * (i / steps);
+    //   final shiftedPath = innerPath.shift(stepOffset);
+    //   totalPath = Path.combine(PathOperation.union, totalPath, shiftedPath);
+    // }
+
+    /// TODO try different BlurStyle?
+    canvas.drawPath(innerPath.shift(shadow.offset), shadow.toPaint());
+  }
+
+  @override
+  bool shouldRepaint(covariant ShapeShadowPainter oldDelegate) {
+    return this != oldDelegate;
+  }
+
+  @override
+  bool operator==(covariant ShapeShadowPainter other) {
+    return shape != other.shape || shadow != other.shadow || rectOverride != other.rectOverride;
+  }
+
+  @override
+  int get hashCode => Object.hashAll([shape, shadow, rectOverride]);
 }

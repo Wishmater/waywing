a5f1ce2c6e365fca3edd14c2106d3afda1df01b3
ross96D <abelaross96@gmail.com>
2025-10-31 18:45:06 -0400

    packages: move xdg_icons from dart pub managed dep to vendoring

---
+++ vendor/xdg_icon/README.md
@@ -0,0 +1,17 @@
+# XDG Icons
+
+[![pub](https://img.shields.io/pub/v/xdg_icons.svg)](https://pub.dev/packages/xdg_icons)
+[![license: MPL](https://img.shields.io/badge/license-MPL-magenta.svg)](https://opensource.org/licenses/MPL-2.0)
+[![CI](https://github.com/canonical/ubuntu-flutter-plugins/workflows/CI/badge.svg)](https://github.com/canonical/ubuntu-flutter-plugins/actions/workflows/ci.yaml)
+[![codecov](https://codecov.io/gh/canonical/ubuntu-flutter-plugins/branch/main/graph/badge.svg)](https://codecov.io/gh/canonical/ubuntu-flutter-plugins)
+
+[XDG theme icons](https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html) for Flutter.
+
+```dart
+XdgIcon(
+  name: 'computer',
+  size: 48,
+)
+```
+
+![XDG Icons](https://raw.githubusercontent.com/canonical/ubuntu-flutter-plugins/main/packages/xdg_icons/images/xdg_icons.png "XDG Icons")
---
+++ vendor/xdg_icon/lib/src/icon.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'package:jovial_svg/jovial_svg.dart';
+import 'package:path/path.dart' as path;
+
+import 'package:xdg_icons/src/data.dart';
+import 'package:xdg_icons/src/platform_interface.dart';
+import 'package:xdg_icons/src/theme.dart';
+
+typedef IconNotFoundBuilder = Widget Function();
+
+class XdgIcon extends StatefulWidget {
+  const XdgIcon({
+    required this.name,
+    this.size,
+    this.scale,
+    this.theme,
+    this.iconNotFoundBuilder,
+    super.key,
+  });
+
+  final String name;
+  final int? size;
+  final int? scale;
+  final String? theme;
+  final IconNotFoundBuilder? iconNotFoundBuilder;
+
+  @override
+  State<XdgIcon> createState() => XdgIconState();
+}
+
+class XdgIconState extends State<XdgIcon> {
+  XdgIconData? _icon;
+  StreamSubscription<dynamic>? _themeChange;
+  bool _iconNotFound = false;
+
+  XdgIconData? get icon => _icon;
+
+  int _resolveSize() {
+    final size = widget.size ?? XdgIconTheme.of(context).size;
+    if (size == null) {
+      throw FlutterError.fromParts(<DiagnosticsNode>[
+        ErrorSummary('Unable to resolve $widget size.'),
+        ErrorDescription('Neither XdgIcon nor XdgIconTheme has size.'),
+        ErrorHint(
+          'Possible solutions:\n\n'
+          '  XdgIcon(\n'
+          '    name: \'${widget.name}\',\n'
+          '    size: 48 // <==\n'
+          '  )\n'
+          '\n'
+          '  XdgIconTheme(\n'
+          '    data: XdgIconThemeData(size: 48), // <==\n'
+          '    child: XdgIcon(\n'
+          '      name: \'${widget.name}\'\n'
+          '    )\n'
+          '  )\n',
+        ),
+      ]);
+    }
+    return size;
+  }
+
+  Future<void> _lookupIcon() {
+    final themeData = XdgIconTheme.of(context);
+    return XdgIconsPlatform.instance
+        .lookupIcon(
+          name: widget.name,
+          size: _resolveSize(),
+          scale: widget.scale ?? themeData.scale,
+          theme: widget.theme ?? themeData.theme,
+        )
+        .then(_updateIcon);
+  }
+
+  void _updateIcon(XdgIconData? icon) {
+    if (_icon == icon || !mounted) {
+      if (icon == null) {
+        setState(() => _iconNotFound = true);
+      }
+      return;
+    }
+    setState(() {
+      _icon = icon;
+      _iconNotFound = icon == null;
+    });
+  }
+
+  void _listenThemeChanges() {
+    if (widget.theme == null && XdgIconTheme.of(context).theme == null) {
+      _themeChange ??=
+          XdgIconsPlatform.instance.onDefaultThemeChanged.listen((dynamic _) {
+        if (mounted) _lookupIcon();
+      });
+    } else {
+      _themeChange?.cancel();
+      _themeChange = null;
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _lookupIcon();
+    _listenThemeChanges();
+  }
+
+  @override
+  void didUpdateWidget(covariant XdgIcon oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.name != oldWidget.name ||
+        widget.size != oldWidget.size ||
+        widget.scale != oldWidget.scale ||
+        widget.theme != oldWidget.theme) {
+      _lookupIcon();
+      _listenThemeChanges();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _themeChange?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final file = File(_icon?.fileName ?? '');
+    final size = _resolveSize();
+    if (file.existsSync()) {
+      if (_icon!.isScalable) {
+        return SizedBox(
+          width: size.toDouble(),
+          height: size.toDouble(),
+          child: ScalableImageWidget.fromSISource(
+            si: ScalableImageSource.fromSvgFile(
+              file.path,
+              file.readAsStringSync,
+              warnF: (_) {},
+            ),
+          ),
+        );
+      }
+
+      return Image.file(
+        file,
+        width: size.toDouble(),
+        height: size.toDouble(),
+      );
+    } else if (_icon?.data != null) {
+      if (_icon!.isScalable) {
+        return SizedBox(
+          width: size.toDouble(),
+          height: size.toDouble(),
+          child: ScalableImageWidget.fromSISource(
+            si: ScalableImageSource.fromSvgFile(
+              file.path,
+              () => utf8.decode(_icon!.data!),
+              warnF: (_) {},
+            ),
+          ),
+        );
+      }
+
+      return Image.memory(
+        Uint8List.fromList(_icon!.data!),
+        width: size.toDouble(),
+        height: size.toDouble(),
+      );
+    } else if (_iconNotFound && widget.iconNotFoundBuilder != null) {
+      return widget.iconNotFoundBuilder!();
+    }
+    return SizedBox.square(dimension: size.toDouble());
+  }
+}
+
+extension _XdgIconDataX on XdgIconData {
+  bool get isScalable => path.extension(fileName).toLowerCase() == '.svg';
+}

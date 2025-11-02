c2939cb3c9bba543eee68c0dbb45ca1ba00327ae
David Alcalde <david.alcalde.cu@gmail.com>
2025-11-02 14:35:05 -0500

    notification_manager: add scrollbar

--- lib/modules/notification/notification_manager_popover.dart
+++ lib/modules/notification/notification_manager_popover.dart
@@ -6,6 +6,7 @@ import "package:waywing/modules/notification/notification_service.dart";
 import "package:waywing/modules/notification/notification_widget.dart";
 import "package:waywing/modules/notification/spec/notifications.dart";
 import "package:waywing/util/derived_value_notifier.dart";
+import "package:waywing/widgets/opacity_gradient.dart";
 
 class NotificationManagerPopover extends StatelessWidget {
   const NotificationManagerPopover({super.key, required this.service});
@@ -14,6 +15,7 @@ class NotificationManagerPopover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scrollController = ScrollController();
     return ConstrainedBox(
       constraints: BoxConstraints(
         minWidth: 256,
@@ -35,12 +37,20 @@ class NotificationManagerPopover extends StatelessWidget {
                 ),
               );
             }
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: notifications.length,
-              itemBuilder: (context, index) {
-                return _NotificationWidget(notifications[index].value);
-              },
+            return Scrollbar(
+              controller: scrollController,
+              child: ScrollOpacityGradient(
+                scrollController: scrollController,
+                maxSize: 32,
+                child: ListView.builder(
+                  controller: scrollController,
+                  shrinkWrap: true,
+                  itemCount: notifications.length,
+                  itemBuilder: (context, index) {
+                    return _NotificationWidget(notifications[index].value);
+                  },
+                ),
+              ),
             );
           },
         ),

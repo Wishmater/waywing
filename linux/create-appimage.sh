405f31f8ac6356da4abd66c47f765464435269fc
ross96D <abelaross96@gmail.com>
2025-11-23 11:43:27 -0500

    appimage: pass build mode param

--- linux/create-appimage.sh
+++ linux/create-appimage.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+set -x
+
+# Set default build type to release
+BUILD_TYPE="release"
+
+# Parse build type
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --release)
+            BUILD_TYPE="release"
+            shift
+            ;;
+        --profile)
+            BUILD_TYPE="profile"
+            shift
+            ;;
+        --debug)
+            BUILD_TYPE="debug"
+            shift
+            ;;
+        *)
+            # Preserve all other arguments for appimagetool
+            break
+            ;;
+    esac
+done
+
 APPIMAGETOOL=""
 if ! type appimagetool > /dev/null; then
     ARCH="$(uname -m)"
@@ -13,7 +40,7 @@ fi
 echo "appimagetoo located in $APPIMAGETOOL"
 
 rm -rf AppDir
-cp -r build/linux/x64/release/bundle AppDir
+cp -r build/linux/x64/$BUILD_TYPE/bundle AppDir
 mv AppDir/waywing AppDir/AppRun
 cp linux/waywing.desktop AppDir
 cp linux/waywing.svg AppDir
